c3f3ab93a932b8a6ee83f49bd7562c60879dbe5c
Zr1RAM <sriram.varadarajulu@gmail.com>
2021-10-09 18:01:10 +0530

    Unusual but working dummy output

--- src/App.js
+++ src/App.js
@@ -1,80 +1,44 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import * as THREE from "three";
-// class App extends Component {
-//     componentDidMount() {
-//         var scene = new THREE.Scene();
-//         var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-//         var renderer = new THREE.WebGLRenderer();
-//         renderer.setSize( window.innerWidth, window.innerHeight );
-//         this.mount.appendChild( renderer.domElement );
-      
-//         var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-//         var material = new THREE.MeshStandardMaterial( { color: 0x7e31eb } );
-//         var cube = new THREE.Mesh( geometry, material );
-//         scene.add( cube );
-//         const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
-//         scene.add( light );
-// camera.position.z = 2;
-// var animate = function () {
-//             requestAnimationFrame( animate );
-//             cube.rotation.x += 0.01;
-//             cube.rotation.y += 0.01;
-//             cube.rotation.z += 0.01;
-//             renderer.render( scene, camera );
-//         };
-//         animate();
-//     }
-//     render() {
-//         return (
-//             <div ref={ref => (this.mount = ref)} />
-//         )
-//     }
-// }
-// const rootElement = document.getElementById("root")
-// ReactDOM.render(<App />, rootElement);
-// export default App;
+import logo from './logo.svg';
+import './App.css';
+import ReactDOM from 'react-dom'
+import React, { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+
+function Box(props) {
+  // This reference will give us direct access to the mesh
+  const mesh = useRef()
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => (mesh.current.rotation.x += 0.01))
+  // Return view, these are regular three.js elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
 
 
 
-
-
-
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import * as THREE from "three";
-// class App extends Component {
-//     render() {
-//         return (
-//             <div />
-//         )
-//     }
-// }
-// const rootElement = document.getElementById("root")
-// ReactDOM.render(<App />, rootElement);
-// export default App;
-
-// import logo from './logo.svg';
-// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
   );
 }
 
